f8257a35b7c97b7a93bfa1ecc61d8fb6f5af05a1
divyansh3214 <divyanshjmi@gmail.com>
2026-06-29 17:11:07 +0000

    control flow

---
+++ javascript/controlflow/one.js
@@ -0,0 +1,15 @@
+let key = 'a';
+switch(key){
+    case 'a':
+        console.log('You pressed A');
+        break;
+    case 'b':
+        console.log('You pressed B');
+        break;
+    default:
+        console.log('You pressed something else');
+}
+const empty={};
+let d=Object.keys(empty).length;
+let val1;
+val1=null ??'default value';
