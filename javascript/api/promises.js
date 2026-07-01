d793152a09883bb4c3a038a8177d71da72d0ea3b
divyansh3214 <divyanshjmi@gmail.com>
2026-07-01 18:35:56 +0000

    promises,async,await ,fwtch done

--- javascript/api/promises.js
+++ javascript/api/promises.js
@@ -1,4 +1,5 @@
 
+
 //promise is a object itself
 //callback function is passed to the promise constructor
 //two parts of callback function are resolve and reject
@@ -28,7 +29,7 @@ promise3.then((values)=>{// all values are passed to the then callback function
 })
 const promise4 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let error = false;
+    let error = true;
     if (!error) {
       resolve([{ name: "John", age: 30 }, { name: "Jane", age: 25 }]);
     } else {
@@ -37,10 +38,57 @@ const promise4 = new Promise((resolve, reject) => {
   }, 1000);
 });
 
-const user = promise4.then((values) => {
+promise4.then((values) => {
   console.log(values);
   return values; // optional: return something from .then
+}).then((username)=>{
+    console.log(username);
+}).catch((error)=>{
+   console.log("Error:", error);
+}).finally(()=>{
+    console.log("Promise is settled");
 });
 
-console.log(user); // logs a Promise immediately
+const promise5 = new Promise((resolve, reject) => {
+  let err = true;
+  setTimeout(() => {
+    if (err) {
+      reject("error: Something went wrong");
+    } else {
+      resolve([{ name: "John", age: 30 }, { name: "Jane", age: 25 }]);
+    }
+  }, 1000);
+});
+
+(async () => {
+  try {
+    const response = await promise5;
+    console.log(response);
+  } catch (error) {
+    console.error("Error:", error); // catches the rejection
+  }
+})();
+
+async function getallusers(){
+  try{
+  const response=await fetch('https://jsonplaceholder.typicode.com/users');
+   const data= await response.json();
+   console.log(data);
+  }
+  catch(error){
+    console.error("Error:", error);
+  }
+}
+getallusers();
+
+fetch('https://jsonplaceholder.typicode.com/users').
+then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.error("Error:", error);
+});
 
