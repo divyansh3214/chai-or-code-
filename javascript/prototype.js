fca8ffca0638eaa1b64a481bed4afa2f1e3b4e02
divyansh3214 <divyanshjmi@gmail.com>
2026-07-02 11:59:08 +0000

    prototyping completed

--- javascript/prototype.js
+++ javascript/prototype.js
@@ -13,3 +13,26 @@ Object.prototype.hitesh=function(){
 
 heropower.hitesh();
 myheros.hitesh();
+
+//inheritance
+const username={
+    username:"hitesh"
+}
+const Teacher={
+    makevideo:true
+}
+
+const TeachingSupport={
+    isavailable:false
+}
+const TASupport={
+    makeAssignment:'JS assignment',
+    fulltime:true,
+    __proto__:TeachingSupport
+}
+Teacher.__proto__=username;
+
+//modern syntax
+Object.setPrototypeOf(Teacher,username);//prototypal inheritance
+
+
